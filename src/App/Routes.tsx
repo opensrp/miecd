2fc8964b0328c9c5a2ea1ea853df24d42d183228
p-netm <pmuriuki98@gmail.com>
2021-03-31 17:26:16 +0300

    Revert type to any for routeProps

--- src/App/Routes.tsx
+++ src/App/Routes.tsx
@@ -243,9 +243,8 @@ export const Routes = (props: RoutesProps) => {
                                 ].map((url) => `${url}${CHILD_PATIENT_DETAIL_URL}`);
                             })()}
                             // tslint:disable-next-line: jsx-no-lambda
-                            component={(routeProps: RouteComponentProps) => (
-                                <ConnectedPatientDetails isChild {...routeProps} />
-                            )}
+                            // eslint-disable-next-line @typescript-eslint/no-explicit-any
+                            component={(routeProps: any) => <ConnectedPatientDetails isChild {...routeProps} />}
                         />
                         <ConnectedPrivateRoute
                             disableLoginProtection={false}
@@ -266,9 +265,8 @@ export const Routes = (props: RoutesProps) => {
                                 );
                             })()}
                             // tslint:disable-next-line: jsx-no-lambda
-                            component={(routeProps: RouteComponentProps) => (
-                                <ConnectedPatientDetails isChild {...routeProps} />
-                            )}
+                            // eslint-disable-next-line @typescript-eslint/no-explicit-any
+                            component={(routeProps: any) => <ConnectedPatientDetails isChild {...routeProps} />}
                         />
                         <ConnectedPrivateRoute
                             disableLoginProtection={false}
