df334b5180eb1d40addc17bd82ae5f8b503e6c4d
p-netm <pmuriuki98@gmail.com>
2021-04-06 17:00:01 +0300

    Add MLS to DataCircle card component
    Removed shallow rendered test suite, it was redundant

--- src/components/DataCircleCard/index.tsx
+++ src/components/DataCircleCard/index.tsx
@@ -2,27 +2,26 @@ import React from 'react';
 import { connect } from 'react-redux';
 import { Link, RouteComponentProps, withRouter } from 'react-router-dom';
 import { Card, CardBody, CardTitle } from 'reactstrap';
+import './index.css';
 import {
-    ALL,
-    INAPPROPRIATELY_FED,
+    RED,
+    PREGNANCY,
     NBC_AND_PNC_CHILD,
     NBC_AND_PNC_WOMAN,
-    NO,
-    NO_RISK,
     NUTRITION,
+    ALL,
+    INAPPROPRIATELY_FED,
+    NO,
     OVERWEIGHT,
-    PREGNANCY,
-    RED,
-    RED_ALERT,
     RISK,
     SEVERE_WASTING,
     STUNTED,
 } from '../../constants';
 import { getLinkToHierarchicalDataTable, Dictionary } from '../../helpers/utils';
-
 import { addFilterArgs } from '../../store/ducks/sms_events';
 import { SmsFilterFunction } from '../../types';
 import './index.css';
+import { useTranslation } from 'react-i18next';
 
 /**
  * interface for props to be passed to DataCircleCard component.
@@ -72,22 +71,23 @@ function DataCircleCard({
     userLocationId,
     permissionLevel,
 }: Props) {
+    const { t } = useTranslation();
     const pregnancyAndPncCircleSpec: CircleSpecProps[] = [
         {
             class: 'red',
-            riskLabel: RED_ALERT,
+            riskLabel: t('Red Alert'),
             riskType: RED,
             riskValue: redAlert,
         },
         {
             class: 'yellow',
-            riskLabel: RISK,
+            riskLabel: t('risk'),
             riskType: RISK,
             riskValue: risk,
         },
         {
             class: 'green',
-            riskLabel: NO_RISK,
+            riskLabel: t('No Risk'),
             riskType: NO,
             riskValue: noRisk,
         },
@@ -96,25 +96,25 @@ function DataCircleCard({
     const nutritionCircleSpec: CircleSpecProps[] = [
         {
             class: 'stunting',
-            riskLabel: 'Stunting',
+            riskLabel: t('Stunting'),
             riskType: STUNTED,
             riskValue: stunting,
         },
         {
             class: 'wasting',
-            riskLabel: 'Wasting',
+            riskLabel: t('Wasting'),
             riskType: SEVERE_WASTING,
             riskValue: wasting,
         },
         {
             class: 'overweight',
-            riskLabel: 'Overweight',
+            riskLabel: t('Overweight'),
             riskType: OVERWEIGHT,
             riskValue: overweight,
         },
         {
             class: 'inappropriate-feeding',
-            riskLabel: 'Inappropriate Feeding',
+            riskLabel: t('Inappropriate Feeding'),
             riskType: INAPPROPRIATELY_FED,
             riskValue: inappropriateFeeding,
         },
--- src/components/DataCircleCard/test/index.test.tsx
+++ src/components/DataCircleCard/test/index.test.tsx
@@ -1,4 +1,3 @@
-import { mount, shallow } from 'enzyme';
 import toJson from 'enzyme-to-json';
 import { createBrowserHistory } from 'history';
 import React from 'react';
@@ -6,25 +5,15 @@ import { Provider } from 'react-redux';
 import { Router } from 'react-router';
 import DataCircleCard from '..';
 import { PREGNANCY } from '../../../constants';
+import { mountWithTranslations } from '../../../helpers/testUtils';
 import store from '../../../store/index';
 
 const history = createBrowserHistory();
 
 describe('DataCircleCard', () => {
-    // eslint-disable-next-line jest/expect-expect
-    it('must render without crashing', () => {
-        const props = { highRisk: 10, lowRisk: 10, noRisk: 10, title: 'test title' };
-        shallow(
-            <Provider store={store}>
-                <Router history={history}>
-                    <DataCircleCard {...props} module={PREGNANCY} />
-                </Router>
-            </Provider>,
-        );
-    });
     it('must render correctly', () => {
         const props = { highRisk: 10, lowRisk: 10, noRisk: 10, title: 'test title' };
-        const wrapper = mount(
+        const wrapper = mountWithTranslations(
             <Provider store={store}>
                 <Router history={history}>
                     <DataCircleCard {...props} module={PREGNANCY} />
