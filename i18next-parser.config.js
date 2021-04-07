57b10486f26f572115f6e8ca0739483b1099f46d
p-netm <pmuriuki98@gmail.com>
2021-04-07 16:02:04 +0300

    Disable some configs in i18n parser
    creating catalogues and using a keyseparator

--- i18next-parser.config.js
+++ i18next-parser.config.js
@@ -4,7 +4,7 @@ module.exports = {
     contextSeparator: '_',
     // Key separator used in your translation keys
 
-    createOldCatalogs: true,
+    createOldCatalogs: false,
     // Save the \_old files
 
     defaultNamespace: 'translation',
@@ -19,7 +19,7 @@ module.exports = {
     keepRemoved: false,
     // Keep keys from the catalog that are no longer in code
 
-    keySeparator: '.',
+    keySeparator: false,
     // Key separator used in your translation keys
     // If you want to use plain english keys, separators such as `.` and `:` will conflict. You might want to set `keySeparator: false` and `namespaceSeparator: false`. That way, `t('Status: Loading...')` will not think that there are a namespace and three separator dots for instance.
 
