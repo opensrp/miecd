e29798507e9615dcc978d1b9b2f16d972641fd36
p-netm <pmuriuki98@gmail.com>
2021-04-12 19:38:53 +0300

    Fix React import in NotFound tests

--- src/components/NotFound/tests/index.test.tsx
+++ src/components/NotFound/tests/index.test.tsx
@@ -1,6 +1,7 @@
 import { mount } from 'enzyme';
 import toJson from 'enzyme-to-json';
 import NotFound from '..';
+import React from 'react';
 
 describe('NotFound', () => {
     it('renders correclty', () => {
