f79f8a917f95ce3883c3ca8c0f5dacc2fe532925
p-netm <pmuriuki98@gmail.com>
2021-04-12 19:26:30 +0300

    Add coverage for tests in villageData

--- src/components/VillageData/tests/index.test.tsx
+++ src/components/VillageData/tests/index.test.tsx
@@ -1,3 +1,4 @@
+import { NBC_AND_PNC_WOMAN, NUTRITION, NBC_AND_PNC_CHILD } from '../../../constants';
 import { shallow } from 'enzyme';
 import toJson from 'enzyme-to-json';
 import { createBrowserHistory } from 'history';
@@ -24,6 +25,7 @@ describe('components/VillageData', () => {
     it('must render correctly with no data', () => {
         const wrapper = mountWithTranslations(<VillageData />);
         expect(toJson(wrapper.find('VillageData Row'))).toMatchSnapshot('Village data with no data');
+        wrapper.unmount();
     });
 
     it('must render correctly with data', () => {
@@ -35,6 +37,89 @@ describe('components/VillageData', () => {
             </Provider>,
         );
         expect(toJson(wrapper.find('tr'))).toMatchSnapshot('VillageData with data');
+        wrapper.unmount();
+    });
+
+    it('pagination works correctly', () => {
+        const props = {
+            ...villageDataProps,
+            module: NUTRITION,
+        };
+        const wrapper = mountWithTranslations(
+            <Provider store={store}>
+                <Router history={history}>
+                    <VillageData {...props} />
+                </Router>
+            </Provider>,
+        );
+
+        expect(wrapper.find('#navrow Pagination').text()).toMatchInlineSnapshot(
+            `"firstfirstpreviousprevious1234nextnextlastlast"`,
+        );
+
+        // inspect entries shown on page 1
+        wrapper.find('tbody#body tr').forEach((td) => {
+            expect(td.text()).toMatchSnapshot('first Page');
+        });
+
+        // go to page 2
+        const page2PaginationItem = wrapper.find('PaginationItem').at(4).find('PaginationLink');
+        expect(toJson(page2PaginationItem)).toMatchSnapshot('paginationItem for page 2');
+
+        page2PaginationItem.simulate('click');
+        wrapper.update();
+
+        // inspect entries shown on page 2
+        wrapper.find('tbody#body tr').forEach((td) => {
+            expect(td.text()).toMatchSnapshot('second Page');
+        });
+        wrapper.unmount();
+    });
+
+    it('works correctly for NBC_AND_PNC_CHILD', () => {
+        const props = {
+            ...villageDataProps,
+            module: NBC_AND_PNC_CHILD,
+        };
+        const wrapper = mountWithTranslations(
+            <Provider store={store}>
+                <Router history={history}>
+                    <VillageData {...props} />
+                </Router>
+            </Provider>,
+        );
+
+        expect(wrapper.find('#navrow Pagination').text()).toMatchInlineSnapshot(
+            `"firstfirstpreviousprevious1234nextnextlastlast"`,
+        );
+
+        wrapper.find('tr').forEach((tr) => {
+            expect(tr.text()).toMatchSnapshot('NBC_AND_PNC_CHILD page');
+        });
+        wrapper.unmount();
+    });
+
+    it('works correctly for NBC_AND_PNC_WOMAN', () => {
+        const props = {
+            ...villageDataProps,
+            module: NBC_AND_PNC_WOMAN,
+        };
+        const wrapper = mountWithTranslations(
+            <Provider store={store}>
+                <Router history={history}>
+                    <VillageData {...props} />
+                </Router>
+            </Provider>,
+        );
+
+        expect(wrapper.find('#navrow Pagination').text()).toMatchInlineSnapshot(
+            `"firstfirstpreviousprevious1234nextnextlastlast"`,
+        );
+
+        wrapper.find('tr').forEach((tr) => {
+            expect(tr.text()).toMatchSnapshot('nBC and PNC woman Page');
+        });
+        wrapper.unmount();
     });
 });
 
