96b2e9c7a414c58172eed499a48ff42db200f70e
Macharia Muguku <machariamuguku@gmail.com>
2021-04-12 20:56:05 +0300

    center items vertically, fixed image width, replace cartoons with images

--- src/containers/pages/Home/index.tsx
+++ src/containers/pages/Home/index.tsx
@@ -2,8 +2,8 @@
 import * as React from 'react';
 import { Link } from 'react-router-dom';
 import pregnancyAvatar from '../../../assets/images/pregnancy.jpg';
-import nbcPncAvatar from '../../../assets/images/carrying-baby.svg';
-import nutritionAvatar from '../../../assets/images/baby-feeding.svg';
+import nbcPncAvatar from '../../../assets/images/pnc_nbc.jpg';
+import nutritionAvatar from '../../../assets/images/nutrition.jpg';
 import { NBC_AND_PNC_URL, NUTRITION_URL, PREGNANCY_URL } from '../../../constants';
 import './index.css';
 
@@ -14,54 +14,56 @@ class Home extends React.Component {
                 <div className="welcome-text">
                     <h1>Welcome to the MIECD dashboard</h1>
                 </div>
-                <div className="components-list">
-                    <div className="spacer">
-                        <img src={pregnancyAvatar} id="pregnancyAvatar" alt="pregnancy module avatar" />
-                        <div className="sub-container-message">
-                            <div id="cont-size">
-                                <h1>Pregnancy</h1>
-                            </div>
-                            <div id="cont-size">
-                                <h4>View the latest message updates about your patients.</h4>
-                            </div>
-                            <div id="cont-sized">
-                                <Link to={PREGNANCY_URL}>
-                                    <button className="button-style default">View</button>
-                                </Link>
-                            </div>
-                        </div>
-                    </div>
-                    <div className="spacer">
-                        <img src={nbcPncAvatar} id="nbcPncAvatar" alt="NBC &#38; PNC module avatar" />
-
-                        <div className="sub-container-message">
-                            <div id="cont-size">
-                                <h1>NBC &#38; PNC</h1>
-                            </div>
-                            <div id="cont-size">
-                                <h4>View the latest message updates about your patients.</h4>
-                            </div>
-                            <div id="cont-sized">
-                                <Link to={NBC_AND_PNC_URL}>
-                                    <button className="button-style default">View</button>
-                                </Link>
+                <div className="center-vertically">
+                    <div className="components-list">
+                        <div className="spacer">
+                            <img src={pregnancyAvatar} id="pregnancyAvatar" alt="pregnancy module avatar" />
+                            <div className="sub-container-message">
+                                <div id="cont-size">
+                                    <h1>Pregnancy</h1>
+                                </div>
+                                <div id="cont-size">
+                                    <h4>View the latest message updates about your patients.</h4>
+                                </div>
+                                <div id="cont-sized">
+                                    <Link to={PREGNANCY_URL}>
+                                        <button className="button-style default">View</button>
+                                    </Link>
+                                </div>
                             </div>
                         </div>
-                    </div>
-                    <div className="spacer">
-                        <img src={nutritionAvatar} id="nutritionAvatar" alt="nutrition module avatar" />
+                        <div className="spacer">
+                            <img src={nbcPncAvatar} id="nbcPncAvatar" alt="NBC &#38; PNC module avatar" />
 
-                        <div className="sub-container-message">
-                            <div id="cont-size">
-                                <h1>Nutrition</h1>
+                            <div className="sub-container-message">
+                                <div id="cont-size">
+                                    <h1>NBC &#38; PNC</h1>
+                                </div>
+                                <div id="cont-size">
+                                    <h4>View the latest message updates about your patients.</h4>
+                                </div>
+                                <div id="cont-sized">
+                                    <Link to={NBC_AND_PNC_URL}>
+                                        <button className="button-style default">View</button>
+                                    </Link>
+                                </div>
                             </div>
-                            <div id="cont-size">
-                                <h4>View the latest message updates about your patients.</h4>
-                            </div>
-                            <div id="cont-sized">
-                                <Link to={NUTRITION_URL}>
-                                    <button className="button-style default">View</button>
-                                </Link>
+                        </div>
+                        <div className="spacer">
+                            <img src={nutritionAvatar} id="nutritionAvatar" alt="nutrition module avatar" />
+
+                            <div className="sub-container-message">
+                                <div id="cont-size">
+                                    <h1>Nutrition</h1>
+                                </div>
+                                <div id="cont-size">
+                                    <h4>View the latest message updates about your patients.</h4>
+                                </div>
+                                <div id="cont-sized">
+                                    <Link to={NUTRITION_URL}>
+                                        <button className="button-style default">View</button>
+                                    </Link>
+                                </div>
                             </div>
                         </div>
                     </div>
