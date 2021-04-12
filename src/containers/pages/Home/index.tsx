1274122226d236ad79141140737a72317a588e03
Macharia Muguku <machariamuguku@gmail.com>
2021-04-12 19:51:02 +0300

    replace pregnancy avatar,style with flexbox

--- src/containers/pages/Home/index.tsx
+++ src/containers/pages/Home/index.tsx
@@ -1,7 +1,7 @@
 // this is the home page component
 import * as React from 'react';
 import { Link } from 'react-router-dom';
-import pregnancyAvatar from '../../../assets/images/pregnant-woman.svg';
+import pregnancyAvatar from '../../../assets/images/pregnancy.jpg';
 import nbcPncAvatar from '../../../assets/images/carrying-baby.svg';
 import nutritionAvatar from '../../../assets/images/baby-feeding.svg';
 import { NBC_AND_PNC_URL, NUTRITION_URL, PREGNANCY_URL } from '../../../constants';
@@ -17,55 +17,51 @@ class Home extends React.Component {
                 <div className="components-list">
                     <div className="spacer">
                         <img src={pregnancyAvatar} id="pregnancyAvatar" alt="pregnancy module avatar" />
-                        <div className="home-sub-containers">
-                            <div className="sub-container-message">
-                                <div id="cont-size">
-                                    <h1>Pregnancy</h1>
-                                </div>
-                                <div id="cont-size">
-                                    <h4>View the latest message updates about your patients.</h4>
-                                </div>
-                                <div id="cont-sized">
-                                    <Link to={PREGNANCY_URL}>
-                                        <button className="button-style default">View</button>
-                                    </Link>
-                                </div>
+                        <div className="sub-container-message">
+                            <div id="cont-size">
+                                <h1>Pregnancy</h1>
+                            </div>
+                            <div id="cont-size">
+                                <h4>View the latest message updates about your patients.</h4>
+                            </div>
+                            <div id="cont-sized">
+                                <Link to={PREGNANCY_URL}>
+                                    <button className="button-style default">View</button>
+                                </Link>
                             </div>
                         </div>
                     </div>
                     <div className="spacer">
                         <img src={nbcPncAvatar} id="nbcPncAvatar" alt="NBC &#38; PNC module avatar" />
-                        <div className="home-sub-containers">
-                            <div className="sub-container-message">
-                                <div id="cont-size">
-                                    <h1>NBC &#38; PNC</h1>
-                                </div>
-                                <div id="cont-size">
-                                    <h4>View the latest message updates about your patients.</h4>
-                                </div>
-                                <div id="cont-sized">
-                                    <Link to={NBC_AND_PNC_URL}>
-                                        <button className="button-style default">View</button>
-                                    </Link>
-                                </div>
+
+                        <div className="sub-container-message">
+                            <div id="cont-size">
+                                <h1>NBC &#38; PNC</h1>
+                            </div>
+                            <div id="cont-size">
+                                <h4>View the latest message updates about your patients.</h4>
+                            </div>
+                            <div id="cont-sized">
+                                <Link to={NBC_AND_PNC_URL}>
+                                    <button className="button-style default">View</button>
+                                </Link>
                             </div>
                         </div>
                     </div>
                     <div className="spacer">
                         <img src={nutritionAvatar} id="nutritionAvatar" alt="nutrition module avatar" />
-                        <div className="home-sub-containers">
-                            <div className="sub-container-message">
-                                <div id="cont-size">
-                                    <h1>Nutrition</h1>
-                                </div>
-                                <div id="cont-size">
-                                    <h4>View the latest message updates about your patients.</h4>
-                                </div>
-                                <div id="cont-sized">
-                                    <Link to={NUTRITION_URL}>
-                                        <button className="button-style default">View</button>
-                                    </Link>
-                                </div>
+
+                        <div className="sub-container-message">
+                            <div id="cont-size">
+                                <h1>Nutrition</h1>
+                            </div>
+                            <div id="cont-size">
+                                <h4>View the latest message updates about your patients.</h4>
+                            </div>
+                            <div id="cont-sized">
+                                <Link to={NUTRITION_URL}>
+                                    <button className="button-style default">View</button>
+                                </Link>
                             </div>
                         </div>
                     </div>
