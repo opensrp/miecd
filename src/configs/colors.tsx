79ad3d29895a031d73ee4a7437e861fa066f4563
p-netm <pmuriuki98@gmail.com>
2021-04-20 21:58:07 +0300

    Refactor Risk coloring on locaface risk

---
+++ src/configs/colors.tsx
@@ -0,0 +1,8 @@
+export const GREEN = '#28a745';
+export type GREEN = typeof GREEN;
+export const YELLOW = '#ffc107';
+export type YELLOW = typeof YELLOW;
+export const RED = '#dc3545';
+export type RED = typeof RED;
+export const GREY = '#6c757d';
+export type GREY = typeof GREY;
