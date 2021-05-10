b2fe5d37c484172499f9d4f9f3423fa24b9a682f
Macharia Muguku <machariamuguku@gmail.com>
2021-05-10 18:06:29 +0300

    add test for missing drill down villages error, fix other errors

--- src/containers/HierarchichalDataTable/test/index.test.tsx
+++ src/containers/HierarchichalDataTable/test/index.test.tsx
@@ -11,7 +11,7 @@ import { fetchLocations } from '../../../store/ducks/locations';
 import { fetchSms } from '../../../store/ducks/sms_events';
 import store from '../../../store/index';
 import { smsSlice } from '../../LogFace/tests/fixtures';
-import { communes, districts, provinces, villages } from './fixtures';
+import { communes, districts, provinces, villages, countryId } from './fixtures';
 
 jest.mock('@fortawesome/react-fontawesome');
 
@@ -34,12 +34,18 @@ describe('HierarchichalDataTable', () => {
         store.dispatch(fetchLocations(villages));
         store.dispatch(fetchSms(smsSlice));
 
-        let props = {
+        // mount with node_id == country id
+        const countryProps = {
             match: {
                 params: {
-                    module: 'pregnancy',
+                    module: 'Pregnancy',
                     risk_highlighter: 'high_risk',
                     title: 'Total Pregnancies',
+                    current_level: '0',
+                    direction: 'down',
+                    node_id: countryId,
+                    permission_level: '0',
+                    from_level: '0',
                 },
             },
         };
@@ -47,79 +53,102 @@ describe('HierarchichalDataTable', () => {
         let wrapper = mountWithTranslations(
             <Provider store={store}>
                 <Router history={history}>
-                    <ConnectedHierarchicalDataTable {...props} />
+                    <ConnectedHierarchicalDataTable {...countryProps} />
                 </Router>
             </Provider>,
         );
-        expect(toJson(wrapper.find('Table'))).toMatchSnapshot('hierarchichalDataTable');
 
-        props = {
+        // find Hanoi province in table
+        const countryRow = wrapper.find('#body').find('tr.cursor-pointer').find('td[children="Hanoi"]');
+        expect(countryRow).toHaveLength(1);
+
+        // mount with node_id == province id for Hanoi
+        const provinceProps = {
             match: {
                 params: {
-                    current_level: 1,
-                    direction: 'down',
-                    module: 'pregnancy',
-                    node_id: '1',
+                    module: 'Pregnancy',
                     risk_highlighter: 'high_risk',
                     title: 'Total Pregnancies',
-                } as any,
+                    current_level: '1',
+                    direction: 'down',
+                    node_id: '78a12165-3c12-471f-8755-c96bac123292',
+                    permission_level: '0',
+                    from_level: '0',
+                },
             },
         };
 
         // pass props in the partern /:risk_highlighter?/:title?/:current_level?/:direction?/:node_id?/:from_level?
-        //             hierarchicaldata/high-risk/156 Total Pregnancies/1/down/1
+        // hierarchicaldata/high-risk/156 Total Pregnancies/1/down/1
         wrapper = mountWithTranslations(
             <Provider store={store}>
                 <Router history={history}>
-                    <ConnectedHierarchicalDataTable {...props} />
+                    <ConnectedHierarchicalDataTable {...provinceProps} />
                 </Router>
             </Provider>,
         );
-        expect(toJson(wrapper.find('tbody'))).toMatchSnapshot('hierarchichalDataTable');
 
-        props = {
+        // find Tay Ho district in table
+        const provinceRow = wrapper.find('#body').find('tr.cursor-pointer').find('td[children="Tay Ho District"]');
+        expect(provinceRow).toHaveLength(1);
+
+        // mount with node_id == district id for Tay Ho District
+        const districtProps = {
             match: {
                 params: {
-                    current_level: 2,
-                    direction: 'down',
-                    module: 'pregnancy',
-                    node_id: '6',
+                    module: 'Pregnancy',
                     risk_highlighter: 'high_risk',
                     title: 'Total Pregnancies',
-                } as any,
+                    current_level: '2',
+                    direction: 'down',
+                    node_id: '623b644d-a1f2-4c5e-b065-d60c0ae6501f',
+                    permission_level: '0',
+                    from_level: '1',
+                },
             },
         };
+
         wrapper = mountWithTranslations(
             <Provider store={store}>
                 <Router history={history}>
-                    <ConnectedHierarchicalDataTable {...props} />
+                    <ConnectedHierarchicalDataTable {...districtProps} />
                 </Router>
             </Provider>,
         );
-        expect(toJson(wrapper.find('tbody'))).toMatchSnapshot('hierarchichalDataTable');
 
-        props = {
+        // find Yen Phu Commune in table
+        const districtRow = wrapper.find('#body').find('tr.cursor-pointer').find('td[children="Yen Phu Commune"]');
+        expect(districtRow).toHaveLength(1);
+
+        // mount with node_id == commune id for Yen Phu Commune
+        const communeProps = {
             match: {
                 params: {
-                    current_level: 3,
-                    direction: 'down',
-                    module: 'pregnancy',
-                    node_id: '13',
+                    module: 'Pregnancy',
                     risk_highlighter: 'high_risk',
                     title: 'Total Pregnancies',
-                } as any,
+                    current_level: '3',
+                    direction: 'down',
+                    node_id: '46d98781-7cc4-4c28-8379-a3552a57acfe',
+                    permission_level: '0',
+                    from_level: '2',
+                },
             },
         };
+
         wrapper = mountWithTranslations(
             <Provider store={store}>
                 <Router history={history}>
-                    <ConnectedHierarchicalDataTable {...props} />
+                    <ConnectedHierarchicalDataTable {...communeProps} />
                 </Router>
             </Provider>,
         );
-        expect(toJson(wrapper.find('tbody'))).toMatchSnapshot('hierarchichalDataTable');
 
-        props = {
+        // find Yen Phu Village in table
+        const villageRow = wrapper.find('#body').find('tr').at(0).find('td[children="Yen Phu Village"]');
+        expect(villageRow).toHaveLength(1);
+
+        let props = {
             match: {
                 params: {
                     current_level: 2,
@@ -277,5 +306,34 @@ describe('HierarchichalDataTable', () => {
         window.history.go = mockFunction;
         wrapper.find('.back-page').simulate('click');
         expect(mockFunction).toBeCalledWith(-1);
+    });
+
+    it('shows friendly error if drill down level is unavailable', () => {
+        // mount level 3 for commune node_id with no villages
+        const props = {
+            match: {
+                params: {
+                    module: 'Pregnancy',
+                    risk_highlighter: 'all',
+                    title: 'Total Pregnancies',
+                    current_level: '3',
+                    direction: 'down',
+                    node_id: '18ee2dfd-8a59-412d-943f-b9fd10ca4209',
+                    permission_level: '0',
+                    from_level: '2',
+                },
+            },
+        };
+
+        const wrapper = mountWithTranslations(
+            <Provider store={store}>
+                <Router history={history}>
+                    <ConnectedHierarchicalDataTable {...props} />
+                </Router>
+            </Provider>,
+        );
+
+        // expect to find the no rows tr and the error message
+        expect(toJson(wrapper.find('#no-rows'))).toMatchSnapshot('error if drill down villages unavailable');
     });
 });
