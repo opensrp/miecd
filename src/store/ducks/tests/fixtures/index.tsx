57a1eb7c3c5ca0c4b3a10b56b83b1be14399e781
p-netm <pmuriuki98@gmail.com>
2021-05-12 10:55:50 +0300

    Add fixtures for logface events

--- src/store/ducks/tests/fixtures/index.tsx
+++ src/store/ducks/tests/fixtures/index.tsx
@@ -1,5 +1,9 @@
 import * as smsDataJson from './smsEvents.json';
+import * as nbcPncReportsJson from './nbcPncReports.json';
+import * as pregnancyReportJson from './pregnancyLogFaceReports.json';
 import superset from '@onaio/superset-connector';
 import { SmsData } from 'store/ducks/sms_events';
 
 export const smsDataFixture = superset.processData(smsDataJson) as SmsData[];
+export const nbcPncSmsFixtures = superset.processData(nbcPncReportsJson);
+export const PregnancyReportFixture = superset.processData(pregnancyReportJson);
