d790a745df356ceabd84089b1c36e6d1582397e7
p-netm <pmuriuki98@gmail.com>
2021-05-12 11:57:09 +0300

    Replance nbcReports fixtures with one for nutritions

--- src/store/ducks/tests/fixtures/index.tsx
+++ src/store/ducks/tests/fixtures/index.tsx
@@ -1,9 +1,9 @@
 import * as smsDataJson from './smsEvents.json';
-import * as nbcPncReportsJson from './nbcPncReports.json';
+import * as nutritionReportsJson from './nutritionReports.json';
 import * as pregnancyReportJson from './pregnancyLogFaceReports.json';
 import superset from '@onaio/superset-connector';
 import { SmsData } from 'store/ducks/sms_events';
 
 export const smsDataFixture = superset.processData(smsDataJson) as SmsData[];
-export const nbcPncSmsFixtures = superset.processData(nbcPncReportsJson);
+export const nutritionSmsFixtures = superset.processData(nutritionReportsJson);
 export const PregnancyReportFixture = superset.processData(pregnancyReportJson);
