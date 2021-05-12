1b2b09962b57007f10534e9abd43158ea05ce9d9
p-netm <pmuriuki98@gmail.com>
2021-05-12 19:55:33 +0300

    Refactor RiskColoring to use new risk filter settings

--- src/components/RiskColoring/index.tsx
+++ src/components/RiskColoring/index.tsx
@@ -1,32 +1,37 @@
 import { useTranslation } from 'react-i18next';
-import { PREGNANCY_MODULE } from '../../constants';
-import { riskCategories } from 'configs/settings';
-import { keyBy } from 'lodash';
+import { pregnancyModuleRiskFilterLookup } from 'configs/settings';
+import { values } from 'lodash';
 import { GREY } from 'configs/colors';
 import React from 'react';
 import './index.css';
 
 interface RiskColoringProps {
     risk: string;
+    displayValue: string;
 }
 
 const riskColoringDefaultProps: RiskColoringProps = {
     risk: '',
+    displayValue: '',
 };
 
 /** Colour codes risk levels */
 const RiskColoring = (props: RiskColoringProps) => {
-    const { risk } = props;
+    const { risk, displayValue } = props;
     const { t } = useTranslation();
 
-    const module = PREGNANCY_MODULE;
-    const categories = riskCategories(t)[module];
-    const categoriesByRisk = keyBy(categories, (item) => item.value);
+    const categories = pregnancyModuleRiskFilterLookup(t);
+    const thisCategory = values(categories).filter((cat) => cat.filterValue.includes(riskLevel));
+    let color = GREY;
+    if (thisCategory.length) {
+        const first = thisCategory[0];
+        color = first.color;
+    }
 
     return (
-        <span style={{ backgroundColor: categoriesByRisk[risk]?.color ?? GREY }} className="badge">
+        <span style={{ backgroundColor: color }} className="badge">
             <div>
-                <p>{categoriesByRisk[risk]?.label ?? t('Not set')}</p>
+                <p>{displayValue ? displayValue : risk}</p>
             </div>
         </span>
     );
