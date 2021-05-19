0f7ab77970968d831df09bcd28daa65fc33c8a5b
Macharia Muguku <machariamuguku@gmail.com>
2021-05-19 13:28:47 +0300

    add react query, upgrade react-router-dom and it's types

--- src/index.tsx
+++ src/index.tsx
@@ -4,6 +4,7 @@ import { ConnectedRouter } from 'connected-react-router';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
+import { QueryClient, QueryClientProvider } from 'react-query';
 import App from './App/App';
 import './index.css';
 import * as serviceWorker from './serviceWorker';
@@ -11,11 +12,15 @@ import store from './store';
 import Ripple from './components/page/Loading';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
+const queryClient = new QueryClient();
+
 ReactDOM.render(
     <React.Suspense fallback={Ripple}>
         <Provider store={store}>
             <ConnectedRouter history={history}>
-                <App />
+                <QueryClientProvider client={queryClient}>
+                    <App />
+                </QueryClientProvider>
             </ConnectedRouter>
         </Provider>
     </React.Suspense>,
