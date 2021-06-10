9aa924df11873c51fa7ff4e66e8c0ee2bf797c44
p-netm <pmuriuki98@gmail.com>
2021-06-10 11:28:07 +0300

    Add test util to ease wrappign components in queryprovider

--- src/helpers/testUtils.tsx
+++ src/helpers/testUtils.tsx
@@ -3,6 +3,9 @@ import React from 'react';
 import { I18nextProvider } from 'react-i18next';
 import i18n from '../mls';
 import { act } from 'react-dom/test-utils';
+import { QueryClient, QueryClientProvider } from 'react-query';
+
+const queryClient = new QueryClient();
 
 export const mountWithTranslations: typeof mount = (component: React.ReactNode, options?: MountRendererProps) => {
     return mount(<I18nextProvider i18n={i18n}>{component}</I18nextProvider>, options);
@@ -17,3 +20,13 @@ export async function waitForPromises() {
         await new Promise((res) => setImmediate(res));
     });
 }
+
+/** wrapper to add providers when mounting components with tests */
+export const mountWithProviders: typeof mount = (component: React.ReactNode, options?: MountRendererProps) => {
+    return mount(
+        <QueryClientProvider client={queryClient}>
+            <I18nextProvider i18n={i18n}>{component}</I18nextProvider>
+        </QueryClientProvider>,
+        options,
+    );
+};
