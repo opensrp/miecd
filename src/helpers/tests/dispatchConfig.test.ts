c70550aee0de611325ed4cb8d42ba69c930b23f6
p-netm <pmuriuki98@gmail.com>
2021-06-25 14:02:30 +0300

    Fix regressed tests

--- src/helpers/tests/dispatchConfig.test.ts
+++ src/helpers/tests/dispatchConfig.test.ts
@@ -1,9 +1,7 @@
-import './dispatchConfig';
+import '../dispatchConfig';
 import { getAllConfigs } from '@opensrp/pkg-config';
 import i18n from '../../mls';
 import { APP_LOGIN_URL, OPENSRP_REST_API_BASE_URL } from 'configs/settings';
-
-jest.mock('./configs/env');
 
 it('can dispatch configs', () => {
     expect(getAllConfigs()).toMatchObject({
