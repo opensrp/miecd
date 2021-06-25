286865f0ea989cbc13002e9cd2d7dafbc086a6b9
p-netm <pmuriuki98@gmail.com>
2021-06-25 13:52:20 +0300

    Setup i18n globally for test

--- src/setupTests.ts
+++ src/setupTests.ts
@@ -1,5 +1,29 @@
 import Adapter from 'enzyme-adapter-react-16';
 import Enzyme from 'enzyme';
+import { setAllConfigs } from '@opensrp/pkg-config';
+import i18n from 'i18next';
+import { initReactI18next } from 'react-i18next';
+
+const configuredLanguage = `en_core`;
+
+i18n.use(initReactI18next)
+    .init({
+        lng: configuredLanguage,
+        fallbackLng: configuredLanguage,
+        interpolation: { escapeValue: false },
+        returnEmptyString: false,
+        nsSeparator: '::',
+        keySeparator: false,
+        react: {
+            useSuspense: false,
+        },
+    })
+    .catch((err) => err);
+
+setAllConfigs({
+    i18n: i18n,
+    projectLanguageCode: 'core',
+});
 
 global.fetch = require('jest-fetch-mock');
 
